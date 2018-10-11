94bf6a116cf9d6e1e4ce2d0f2c83e21d27ef15b9
klin6 <link6@hawaii.edu>
2018-10-10 22:05:26 -1000

    Task 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Container, Header } from 'semantic-ui-react';
 
 class LuckyBelly extends React.Component {
 
   render() {
     return (
-        <h1>Lucky Belly!</h1>
+        <Container textAlign="center">
+    <Header as='h1'>Lucky Belly!</Header>
+    </Container>
   );
   }
 }
